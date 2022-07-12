2f6abd38eaa39b7faadf5edea317b3896aa289b7
guowei1berry <guowei1berry@gmail.com>
2022-07-12 11:53:21 +0800

    scrolltoviewsection

---
+++ scroll.js
@@ -0,0 +1,11 @@
+
+
+const myFunction = () =>{
+
+    const myElement = document.getElementById("demo");
+
+    let text = myElement.innerText;
+    console.log(text)
+    myElement.innerText ="changed!"
+
+}
